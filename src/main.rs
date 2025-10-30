8c29c3c1bf53fb699e77614bc8a180b9d9c39d8d
Afonso Miguel <afonso.miguel@pucpr.br>
2025-10-30 17:46:47 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #[allow(unused_imports)]
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 #[allow(dead_code)]
 struct MyStruct {
     data: i32,
-    sibling: Option<Rc<RefCell<MyStruct>>>,
+    sibling: Option<Weak<RefCell<MyStruct>>>,
 }
 
 impl Drop for MyStruct {
@@ -24,9 +24,14 @@ fn main() {
         sibling: None,
     }));
 
-    i1.borrow_mut().sibling = Some(Rc::clone(&i2));
-    i2.borrow_mut().sibling = Some(Rc::clone(&i1));
+    i1.borrow_mut().sibling = Some(Rc::downgrade(&i2));
+    i2.borrow_mut().sibling = Some(Rc::downgrade(&i1));
 
     println!("The value of field1 is: {}", i1.borrow().data);
     println!("The value of field1 is: {}", i2.borrow().data);
+
+    match &i1.borrow().sibling{
+        Some(w) => {},
+        None => println!("i1 não tem irmão!") 
+    }
 }
